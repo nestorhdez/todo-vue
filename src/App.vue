555ea41664d89e6e1e5f485b48428f6400d40a30
Nestor hdez <nestorhdez.26@gmail.com>
2019-08-14 15:13:42 +0100

    todo project.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>To do list</h1>
+    <div class="todo-form">
+      <input v-model="newTodo.text" type="text" placeholder="Write a to do..."/>
+      <button @click="postTodo">Add it</button>
+    </div>
+
+    <div class="todos-container">
+      <Todo v-bind:todo="todo" v-bind:key="`t-${i}`" v-for="(todo, i) in todos"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/todo.vue'
+import axios from 'axios'
+
+const baseURL = 'http://localhost:3000/todos'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Todo
+  },
+  data(){
+    return {
+      newTodo: {text : ''},
+      todos: []
+    }
+  },
+  mounted() {
+    this.getTodos();
+  },
+  methods : {
+    getTodos() {
+      axios.get(baseURL)
+        .then(res =>  this.todos = res.data)
+        .catch(err => console.log(err))
+    },
+    postTodo() {
+      axios.post(baseURL, {title : this.newTodo.text})
+        .then(res => {
+          console.log(res.data);
+          this.getTodos();
+        })
+        .catch(err => console.log(err))
+    },
+    delTodo(id) {
+      let todo = this.todos.find(t => t.id == id);
+      axios.delete(todo)
+      .then(res => {
+        console.log(res)
+        this.getTodos();
+      })
+      .catch(err => console.log(err))
+    }
   }
 }
 </script>
 
 <style>
+body {
+  background-color: #2c3e50;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.todo-form {
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+}
+.todo-form input {
+  height: 40px;
+  border-radius: 3px;
+  margin-bottom: 10px;
+}
+.todo-form button {
+  height: 25px;
+}
+.todos-container {
+  width: 400px;
+  margin-top: 50px;
+  background-color: white;
+  color: #2c3e50;
+  border-radius: 3px;
 }
 </style>
---
+++ src/components/todo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="todo">
+    <h2>{{todo.title}}</h2>
+    <button >Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo',
+  props: {
+    todo: {
+      title: String,
+      id: Number
+    },
+    delTodo: Function
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .todo {
+    padding: 10px;
+    border-top: 1px solid #2c3e50;
+  }
+  .todo h2 {
+    margin: 5px 0;
+  }
+</style>
